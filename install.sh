954ca88945524932e280fd08ed2568abb1780963
Joonhyung Shin <joonhyung.shin@gmail.com>
2022-06-30 14:24:48 +0900

    Change install script

--- install.sh
+++ install.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-BUILD=./build
+SOURCE_DIR=./snippets
+BUILD_DIR=./build
 
-mkdir $BUILD || { echo 'delete build directory and try again'; exit 1; }
+mkdir $BUILD_DIR || { echo 'delete build directory and try again'; exit 1; }
 
-for install in $(find ./snippets -name 'install.txt')
+for install in $(find $SOURCE_DIR -name 'install.txt')
 do
-	codedir=$(dirname $install)
+	fullcodedir=$(dirname $install)
+	codedir=${fullcodedir#$SOURCE_DIR/}
 	cat $install | while read code
 	do
 		if [[ ! -z $code ]]
 		then
-			snippet=$BUILD/$code.sublime-snippet
+			mkdir -p $BUILD_DIR/$codedir
+			snippet=$BUILD_DIR/$codedir/$code.sublime-snippet
 			cat <<EOT >> $snippet
 <snippet>
 	<content><![CDATA[
 EOT
-			cat $codedir/$code.cpp >> $snippet
+			cat $fullcodedir/$code.cpp >> $snippet
 			cat <<EOT >> $snippet
 ]]></content>
 	<tabTrigger>$code</tabTrigger>
 	<scope>source.c++</scope>
 </snippet>
 EOT
-			echo "collected $code"
+			echo "collected $codedir/$code"
 		fi
 	done
 done
 
-echo "snippets collected to $BUILD"
+echo "snippets collected to $BUILD_DIR"
 
 SUBLIME_PATH="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 SNIPPET_DIR=algorithms-cpp
@@ -36,7 +39,7 @@ if [ -d "$SUBLIME_PATH" ]
 then
 	rm -rf "$SUBLIME_PATH/$SNIPPET_DIR"
 	mkdir "$SUBLIME_PATH/$SNIPPET_DIR"
-	cp $BUILD/* "$SUBLIME_PATH/$SNIPPET_DIR/"
+	cp -r $BUILD_DIR/* "$SUBLIME_PATH/$SNIPPET_DIR/"
 	echo "snippets installed to $SUBLIME_PATH/$SNIPPET_DIR"
 fi
 
